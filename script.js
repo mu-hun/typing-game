5cdba1846c9f97822ec41c61ec2773a49a2cbf9b
Muhun Kim <iam@muhun.kim>
2024-10-25 14:54:48 +0900

    Improve markup and a11y
    - 게임 초기 상태에서 시작 버튼 자동 초점 맞추기
    - 하이라이트 문자를 클래스네임 스타일링 대신에 <mark> 엘리먼트로 감싸기
    - 게임 종료 후 입력 상자 비활성화

--- script.js
+++ script.js
@@ -7,25 +7,33 @@ let startTime = Date.now()
 const quoteElement = document.getElementById('quote')
 const messageElement = document.getElementById('message')
 const typedValueElement = document.getElementById('typed-value')
+const startButton = document.getElementById('start')
+startButton.focus()
 
-document.getElementById('start').addEventListener('click', () => {
+function setNextWord() {
+  const spanWords = words.map(function (word, index) {
+    return wordIndex === index ? `<mark>${word}</mark>` : `<span>${word}</span>`
+  })
+  quoteElement.innerHTML = spanWords.join('')
+  messageElement.innerText = ''
+  typedValueElement.value = ''
+}
+
+startButton.addEventListener('click', (event) => {
   const quoteIndex = Math.floor(Math.random() * quotes.length)
   const quote = quotes[quoteIndex]
   words = quote.split(' ')
   wordIndex = 0
 
-  const spanWords = words.map(function (word) {
-    return `<span>${word}</span>`
-  })
-  quoteElement.innerHTML = spanWords.join('')
-  quoteElement.childNodes[0].className = 'highlight'
-  messageElement.innerText = ''
-  typedValueElement.value = ''
+  setNextWord()
+
+  typedValueElement.disabled = false
+  event.target.disabled = true
   typedValueElement.focus()
   startTime = new Date().getTime()
 })
 
-typedValueElement.addEventListener('input', () => {
+typedValueElement.addEventListener('input', (event) => {
   const currentWord = words[wordIndex]
   const typedValue = typedValueElement.value
 
@@ -35,16 +43,18 @@ typedValueElement.addEventListener('input', () => {
       elapsedTime / 1000
     } seconds.`
     messageElement.innerText = message
-  } else if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
+    event.target.disabled = true
+    event.target.value = ''
+    startButton.disabled = false
+    startButton.focus()
+    return
+  }
+  if (typedValue.endsWith(' ') && typedValue.trim() === currentWord) {
     typedValueElement.value = ''
     wordIndex++
-    for (const wordElement of quoteElement.childNodes) {
-      wordElement.className = ''
-    }
-    quoteElement.childNodes[wordIndex].className = 'highlight'
-  } else if (currentWord.startsWith(typedValue)) {
-    typedValueElement.className = ''
-  } else {
-    typedValueElement.className = 'error'
+    setNextWord()
+    return
   }
+
+  typedValueElement['aria-invalid'] = !currentWord.startsWith(typedValue)
 })
